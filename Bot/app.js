19d1df2ad65f91e935f0b7ea26249bb7bdd72549
Sam Levy <slevy239@gmail.com>
2020-02-04 23:31:05 -0500

    bot

--- Bot/app.js
+++ Bot/app.js
@@ -4,9 +4,9 @@ const credentials = require('./credentials')
 async function instaBot () {
   const browser = await puppeteer.launch({
       headless: false,
-      args: [
-          '--window-size=1920,1080'
-      ]
+    //   args: [
+    //       '--window-size=1920,1080'
+    //   ]
   });
   const page = await browser.newPage();
   await page.goto('https://www.instagram.com/accounts/login/');
@@ -15,8 +15,27 @@ async function instaBot () {
   await page.type('input[name="password"]', credentials.password);
 
   await page.click('button[type="submit"]');
-  // Add a wait for some selector on the home page to load to ensure the next step works correctly
-//   await page.pdf({path: 'page.pdf', format: 'A4'});
-//   await browser.close();
+
+  await page.waitFor(() => document.querySelector('[placeholder=Search]'))
+
+  await page.evaluate(() => document.querySelector('[href="/accounts/activity/"]').click())
+
+  await page.waitFor(() => document.querySelectorAll('[role=button] ._5f5mN').length)
+
+  await page.evaluate(() => {
+    const elements = document.querySelectorAll('[role=button] ._5f5mN')
+
+    elements.forEach(element => {
+      if (element.innerText === 'Follow') {
+        element.click()
+      }
+    })
+  })
+
+  await page.waitFor(4000)
+
+  await browser.close()
+
+  instaBot()
 };
 instaBot();
